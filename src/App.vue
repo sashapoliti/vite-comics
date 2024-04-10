6e5ee099d439347a62e5dfc49b70ad2e44c7f38a
sashapoliti <sas.politi@gmail.com>
2024-04-10 17:08:17 +0200

    add imgs in public

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <template>
-  
+  <HeaderComponent />
+  <MainComponent />
+  <FooterComponent />
 </template>
 
 <script>
+import HeaderComponent from "./components/HeaderComponent.vue";
+import MainComponent from "./components/MainComponent.vue";
+import FooterComponent from "./components/FooterComponent.vue";
   export default {
-    name : 'App'
+    name : 'App',
+    components : {
+      HeaderComponent,
+      MainComponent,
+      FooterComponent
+    }
   }
 </script>
 
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <main>
+        main
+    </main>
+</template>
+
+<script>
+    export default {
+        name: 'MainComponent'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
