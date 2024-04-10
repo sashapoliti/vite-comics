66d84beb5a50b1ded6dba55dff81210b1c382616
sashapoliti <sas.politi@gmail.com>
2024-04-10 16:11:18 +0200

    init scaffolding

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1 class="text-center">CHARACTERS DC MERCHANDISE</h1>
+    <i class="fa fa-solid fa-home"></i>
+  </div>
+</template>
+
+<script>
+  export default {
+    name : 'App'
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
