fc0a1d38f6475523cc065ce4d253acca39ef5893
sashapoliti <sas.politi@gmail.com>
2024-04-10 18:41:04 +0200

    header component done

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,15 +1,111 @@
 <template>
-    <header>
-        header
-    </header>
+  <header>
+    <div class="container d-flex justify-content-between align-items-center">
+      <div id="logo">
+        <img class="img-fluid" src="/images/dc-logo.png" alt="Logo" />
+      </div>
+      <ul class="d-flex">
+        <li v-for="(item, index) in menu" :key="index">
+          <a class="d-flex align-items-center text-uppercase" :href="item.url">
+            {{ item.name }}
+          </a>
+        </li>
+      </ul>
+    </div>
+  </header>
 </template>
 
 <script>
-    export default {
-        name: 'HeaderComponent'
-    }
+export default {
+  name: "HeaderComponent",
+  data() {
+    return {
+      menu: [
+        {
+          name: "Characters",
+          url: "#",
+        },
+        {
+          name: "Comics",
+          url: "#",
+        },
+        {
+          name: "Movies",
+          url: "#",
+        },
+        {
+          name: "Tv",
+          url: "#",
+        },
+        {
+          name: "Games",
+          url: "#",
+        },
+        {
+          name: "Collectibles",
+          url: "#",
+        },
+        {
+          name: "Videos",
+          url: "#",
+        },
+        {
+          name: "Fans",
+          url: "#",
+        },
+        {
+          name: "News",
+          url: "#",
+        },
+        {
+          name: "Shop",
+          url: "#",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
+@use "../assets/styles/partials/variables" as *;
 
+header {
+  font-family: $anton;
+  font-size: 0.9rem;
+  & > div {
+    height: 130px;
+    div#logo {
+      width: 80px;
+      aspect-ratio: 1 / 1;
+    }
+    ul {
+      li {
+        &:last-child {
+          a {
+            margin-right: 0;
+          }
+        }
+        a {
+          height: 130px;
+          margin-right: 35px;
+          position: relative;
+          &:hover {
+            color: $general-blue;
+            transition-duration: 0.3s;
+            &::after {
+              content: "";
+              position: absolute;
+              left: 0;
+              bottom: 0;
+              width: 100%;
+              height: 5px;
+              background-color: $general-blue;
+            }
+          }
+        }
+      }
+    }
+  }
+}
 </style>
